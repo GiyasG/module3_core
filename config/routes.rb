f32d3d44974759cef39b215e70556f2f295cc7cd
Giyas Guliyev <g.g.giyas@gmail.com>
2018-01-29 15:16:32 +0300

    upload code

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  scope :api, defaults: {format: :json} do
+    resources :cities, except: [:new, :edit]
+  end
+  get '/ui' => 'ui#index'
+  get '/ui#' => 'ui#index'
+  root "ui#index"
+end
